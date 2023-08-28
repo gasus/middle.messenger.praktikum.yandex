95f1f0fc7ea017ba34df536cdcd5bc09a83165cb
Gasu <zgas@mail.ru>
2023-08-28 12:26:54 +0500

    fixes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
+import path from 'path';
 import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@style-variables': path.resolve(__dirname, 'src/style-variables.less')
+        }
+    },
     plugins: [handlebars()],
 }) 
